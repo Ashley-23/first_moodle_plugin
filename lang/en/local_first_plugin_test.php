8b4c00ba1f150c4a08209542d7afdad9b6de6d21
Ashley-23 <ashleysatchivi92@gmail.com>
2022-06-29 15:45:27 +0000

    first commit

---
+++ lang/en/local_first_plugin_test.php
@@ -0,0 +1,28 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     local_first_plugin_test
+ * @category    string
+ * @copyright   2022 Ashley K Y SATCHIVI <ashleysatchivi92@gmail.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+// Je ne veux juste pas supprimé le commentaire.
+defined('MOODLE_INTERNAL') || die();
+// Je ne veux juste pas supprimé la ligne.
+$string['pluginname'] = 'First_plugin_test';
